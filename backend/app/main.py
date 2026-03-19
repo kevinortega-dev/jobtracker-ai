f1528464273c34360f01f20e2aabed0bdc56936a
DataKev-Code <ke.ortega@duocuc.cl>
2026-03-19 18:50:34 -0300

    fix: add production URL to CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,7 +34,7 @@ app = FastAPI(
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://produccion-positiva-de-corazones.up.railway.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
